279dc375e66dff245c5bde183b6d0f4aa0f1bc12
coordova <grcordova@gmail.com>
2022-12-19 18:53:01 -0400

    Build Modern Laravel Apps Using Inertia.js - E5 - Progress Indicators

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { InertiaProgress } from "@inertiajs/progress";
+import { reduce } from "lodash";
 
 createInertiaApp({
     resolve: (name) =>
@@ -13,4 +15,19 @@ createInertiaApp({
             .use(plugin)
             .mount(el);
     },
+});
+
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 250,
+
+    // The color of the progress bar.
+    color: "cyan", // "#29d",
+
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+
+    // Whether the NProgress spinner will be shown.
+    showSpinner: true,
 });
