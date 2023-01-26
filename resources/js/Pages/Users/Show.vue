d50af6c7c1aa206a50ec75b6cdf4823cf39cc27a
coordova <grcordova@gmail.com>
2023-01-25 22:57:24 -0400

    Inertia and SPA Techniques - E3 - Serialization Option1

--- resources/js/Pages/Users/Show.vue
+++ resources/js/Pages/Users/Show.vue
@@ -1,13 +1,12 @@
 <template>
     <layout>
-        <h1 class="text-3xl">Usuario</h1>
-        <hr>
-        <ul>
-            <li>Nombre: {{ user.name }}</li>
-            <li>Nombre: {{ user.email }}</li>
+        <h1 class="text-3xl">{{ user.name }}</h1>
+        <hr class="my-3">
+        <ul class="space-y-2">
+            <li>Email: {{ user.email }}</li>
+            <li>Created At: {{ user.created_at }}</li>
+            <li>{{ user }}</li>
         </ul>
-        <hr>
-        {{ user }}
     </layout>
 </template>
 
